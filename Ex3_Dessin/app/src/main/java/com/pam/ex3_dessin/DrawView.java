ac5277c36fc94598be4eb3e82b4a6a2f8f20443c
Maxence Guidez <202130087@collegealma.ca>
2023-05-01 09:37:26 -0400

    Ex3, add the press, longPress, doublePress, simpleLines.

--- Ex3_Dessin/app/src/main/java/com/pam/ex3_dessin/DrawView.java
+++ Ex3_Dessin/app/src/main/java/com/pam/ex3_dessin/DrawView.java
@@ -1,30 +1,150 @@
 package com.pam.ex3_dessin;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.view.GestureDetectorCompat;
+
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class DrawView extends View {
 
-    private Paint linePaint;
-    private Paint longPressPaint;
     private Paint touchPaint;
-    private Paint flingPaint;
+    private Paint longPressPaint;
     private Paint doubleTapPaint;
+    private Paint linePaint;
+    private Paint flingPaint;
 
-    public void clear() {
-        elements.clear();
-        current = null;
+    private List<Point> points = new ArrayList<>();
+    private List<Dot> dots = new ArrayList<>();
+    private List<Polyline> lines = new ArrayList<>();
+    private Polyline current;
+    private final GestureDetectorCompat detector;
+
+    public DrawView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+        initializePaints();
+
+        detector = new GestureDetectorCompat(
+                this.getContext(),
+                new GestureDetector.SimpleOnGestureListener(){
+                    @Override
+                    public boolean onSingleTapConfirmed(@NonNull MotionEvent e) {
+                        dots.add(new Dot(e, touchPaint));
+                        invalidate();
+
+                        return true;
+                    }
+
+                    @Override
+                    public void onLongPress(@NonNull MotionEvent e) {
+                        dots.add(new Dot(e, longPressPaint));
+                        invalidate();
+                    }
+
+                    @Override
+                    public boolean onDoubleTap(@NonNull MotionEvent e) {
+                        dots.add(new Dot(e, doubleTapPaint));
+                        invalidate();
+
+                        return true;
+                    }
+                }
+        );
     }
 
-    List<CanvasElement> elements = new ArrayList<>();
-    Polyline current;
+    public void initializePaints(){
+        touchPaint = new Paint();
+        touchPaint.setColor(Color.BLUE);
 
+        longPressPaint = new Paint();
+        longPressPaint.setColor(Color.GREEN);
 
-    public DrawView(Context context) {
-        super(context);
+        doubleTapPaint = new Paint();
+        doubleTapPaint.setColor(Color.MAGENTA);
+
+        linePaint = new Paint();
+        linePaint.setColor(Color.BLACK);
+        linePaint.setStrokeWidth(10);
+
+        flingPaint = new Paint();
+        flingPaint.setColor(Color.RED);
+        flingPaint.setStrokeWidth(15);
+    }
+
+    public void clear() {
+        lines.clear();
+        points.clear();
+        dots.clear();
+        current = null;
+
+        invalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        for(Dot p : dots){
+            p.drawOn(canvas);
+        }
+
+        for(Polyline pl : lines){
+            Point lastPoint = null;
+
+            for(Point pointInLine : pl.points){
+                if(pointInLine != pl.points.get(0)){
+                    canvas.drawLine(lastPoint.x, lastPoint.y, pointInLine.x, pointInLine.y, linePaint);
+                }
+                lastPoint = pointInLine;
+            }
+        }
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        boolean consumed = detector.onTouchEvent(event);
+
+        if(!consumed){
+            switch (event.getAction()){
+                case MotionEvent.ACTION_DOWN:
+                    points.add(new Point(event));
+                    invalidate();
+                    break;
+
+                case MotionEvent.ACTION_MOVE:
+                    if(current == null){
+                        Paint bl = new Paint();
+                        bl.setColor(Color.BLACK);
+                        current = new Polyline(event, bl);
+                        lines.add(current);
+                    }
+                    else{
+                        current.add(event);
+                    }
+                    invalidate();
+                    break;
+
+                case MotionEvent.ACTION_UP:
+                    invalidate();
+
+                    current = null;
+                    break;
+            }
+        }
+
+        return true;
     }
 }
--- Ex3_Dessin/app/src/main/java/com/pam/ex3_dessin/MainActivity.java
+++ Ex3_Dessin/app/src/main/java/com/pam/ex3_dessin/MainActivity.java
@@ -1,6 +1,8 @@
 package com.pam.ex3_dessin;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnClear = findViewById(R.id.btnClear);
+        DrawView drawView = findViewById(R.id.drawView);
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawView.clear();
+            }
+        });
     }
 }
